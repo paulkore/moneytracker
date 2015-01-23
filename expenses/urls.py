301503dc4e1909463ceff285ecb5d90ff65a11ad
paulkore <paulkore@outlook.com>
2015-01-23 15:55:13 -0500

    Remove legacy Person views
    Issue #1:
    I was originally planning to re-purpose these for user account related
    views, but given the special nature of user accounts might as well
    create these from scratch.

--- expenses/urls.py
+++ expenses/urls.py
@@ -12,14 +12,6 @@ urlpatterns = patterns(
     url(r'^(?P<event_name_slug>[-\w]+)/$', views.EventRecordsView.as_view(), name='event-records'),
     url(r'^(?P<event_name_slug>[-\w]+)/record/add/$', views.expense_form_view, name='event-record-create'),
     url(r'^(?P<event_name_slug>[-\w]+)/record/(?P<record_id>\d+)/$', views.expense_form_view, name='event-record-edit')
-
-    # TODO: re-purpose these to user registration / user profile editing
-    #
-    # url(r'^person/add/$', views.PersonCreateView.as_view(), name='person-create'),
-    # url(r'^person/(?P<pk>\d+)/$', views.PersonDetailView.as_view(), name='person-detail'),
-    # url(r'^person/(?P<pk>\d+)/update/$', views.PersonUpdateView.as_view(), name='person-update'),
-    # url(r'^person/(?P<pk>\d+)/delete/$', views.PersonDeleteView.as_view(), name='person-delete'),
-
 )
 
 
--- expenses/views/__init__.py
+++ expenses/views/__init__.py
@@ -1,11 +1,3 @@
 from expenses.views.event_records_view import EventRecordsView
-
 from expenses.views.money_record_view import expense_form_view
-
-# TODO: re-purpose these for user account creation / profile editing
-#
-# from expenses.views.person_view import PersonDetailView
-# from expenses.views.person_view import PersonCreateView
-# from expenses.views.person_view import PersonUpdateView
-# from expenses.views.person_view import PersonDeleteView
 
