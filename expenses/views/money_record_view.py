e4bcb862298a2ccd2b84e7d302fcc1da53119ebc
paulkore <paulkore@outlook.com>
2015-01-26 19:23:22 -0500

    Add user authentication check in money record view
    Temporarily raises an exception until a redirect to login is implemented
    Related to issue #1

--- expenses/views/money_record_view.py
+++ expenses/views/money_record_view.py
@@ -6,6 +6,11 @@ from expenses.models import Event, MoneyRecord
 
 
 def money_record_view(request, event_name_slug, record_id=None):
+    user = request.user
+    if not user.is_authenticated():
+        # TODO: redirect to login page instead
+        raise Exception('User is not authenticated')
+
     event = Event.find_by_name_slug(event_name_slug)
     assert event
 
