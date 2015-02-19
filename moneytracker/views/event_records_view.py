be0566d395200a0e14ee0e54f3bc041bed668669
paulkore <paulkore@outlook.com>
2015-02-19 14:37:42 -0500

    Implement concept of "allocations" in table view
    Issue #28:
    
    This affects the table view (visible in wide viewports)
    
    - New column "Paid by" that displays the name of the payer
    - Column "Amount" now displays the nominal amount of the record
    - Original column "Amount" renamed to "Effective" because it displays
    the amount that's effective towards the total cost of the event
    
    Additional view for the "Participants" section: expense allocations, to
    complement the existing "contributions" view.
    
    The allocation values are present, but are currently hidden.
    A toggle control will be implemented in the near future to toggle
    between the allocation and the contribution views.

--- moneytracker/views/event_records_view.py
+++ moneytracker/views/event_records_view.py
@@ -4,11 +4,14 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from moneytracker.auth import has_event_access
-from moneytracker.models import Event
+from moneytracker.models import Event, MoneyRecord, Participant
 
 
-class MoneyRecordWrapper:
-    def __init__(self, money_record):
+class MoneyRecordData:
+    def __init__(self, money_record, participants):
+        assert type(money_record) is MoneyRecord
+        assert len(participants) > 0 and type(participants[0]) is Participant
+
         self.record_id = money_record.id
         self.description = money_record.description
         self.pub_date = money_record.pub_date
@@ -16,18 +19,25 @@ class MoneyRecordWrapper:
         self.participant1 = money_record.participant1
         self.participant2 = money_record.participant2
 
+        self.contributions = {}
+        self.allocations = {}
+
         assert money_record.participant1
         if money_record.participant2 is None:
-            self.contributions = {
-                money_record.participant1: self.amount
-            }
+            # expense record
+
             self.total_amount = self.amount
+            self.contributions[money_record.participant1] = self.amount
+
+            individual_allocation = self.amount / len(participants)
+            for participant in participants:
+                self.allocations[participant] = individual_allocation
         else:
-            self.contributions = {
-                money_record.participant1: +self.amount,
-                money_record.participant2: -self.amount,
-            }
+            # transfer record
+
             self.total_amount = Decimal(0)
+            self.contributions[money_record.participant1] = +self.amount
+            self.contributions[money_record.participant2] = -self.amount
 
         self.url_edit = reverse('edit-record',
                                 kwargs={'event_name_slug': money_record.event.name_slug, 'record_id': money_record.id})
@@ -49,16 +59,20 @@ def event_records_view(request, event_name_slug):
 
         event_total = Decimal(0)
         participant_contribution = {}
+        participant_allocation = {}
         for p in participants:
             participant_contribution[p] = Decimal(0)
+            participant_allocation[p] = Decimal(0)
 
-        money_record_items = []
+        money_record_data_items = []
         for money_record in money_records:
-            money_record_view_item = MoneyRecordWrapper(money_record)
-            money_record_items.append(money_record_view_item)
-            event_total += money_record_view_item.total_amount
-            for p in money_record_view_item.contributions:
-                participant_contribution[p] += money_record_view_item.contributions[p]
+            money_record_data = MoneyRecordData(money_record, participants)
+            money_record_data_items.append(money_record_data)
+            event_total += money_record_data.total_amount
+            for p in money_record_data.contributions:
+                participant_contribution[p] += money_record_data.contributions[p]
+            for p in money_record_data.allocations:
+                participant_allocation[p] += money_record_data.allocations[p]
 
         event_split = event_total / len(participants)
         participant_variance = {}
@@ -78,10 +92,11 @@ def event_records_view(request, event_name_slug):
         return render(request, 'event_records.html', {
             'event': event,
             'participants': participants,
-            'money_records': money_record_items,
+            'money_records': money_record_data_items,
             'event_total': event_total,
             'event_split': event_split,
             'participant_contribution': participant_contribution,
+            'participant_allocation': participant_allocation,
             'participant_variance': participant_variance,
             'participant_overcontrib': participant_overcontrib,
             'participant_undercontrib': participant_undercontrib,
