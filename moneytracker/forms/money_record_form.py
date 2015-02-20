0c99590a3df721b9b04373030b166d3cb661abae
paulkore <paulkore@outlook.com>
2015-02-20 18:54:11 -0500

    Add allocations selector to the expense record form
    Issue #32:
    Up to now, it was assumed that all expenses apply to the entire group.
    This commit adds a new field to the expense form: allocations (labeled
    "Covers", as in, the expense covers participants X, Y, Z)
    The user can toggle between two modes: "entire group" and "select
    participants"
    The second mode brings up a list of check-boxes labeled with the
    participant names; The user can select any non-empty combination of
    participants.
    Combinations of size 1 are allowed, which denotes personal expenses.

--- moneytracker/forms/money_record_form.py
+++ moneytracker/forms/money_record_form.py
@@ -12,12 +12,34 @@ from moneytracker.models import MoneyRecord, Event, Participant, Allocation
 class MoneyRecordForm(forms.Form):
 
     # field declarations
-    date = forms.DateField(label='Date', input_formats=['%Y/%m/%d', '%Y-%m-%d', '%m/%d/%Y'], widget=DateInput())
-    amount = forms.DecimalField(label='Amount', decimal_places=2)
-    description = forms.CharField(label='Description', max_length=200)
-    participant1 = forms.ChoiceField(label='Paid for expense', choices=[])
-    participant2 = forms.ChoiceField(label='N/A', choices=[], required=False)
     record_type_hidden = forms.CharField(widget=forms.HiddenInput())
+
+    date = forms.DateField(label='Date',
+                           input_formats=['%Y/%m/%d', '%Y-%m-%d', '%m/%d/%Y'],
+                           widget=DateInput(),
+                           required=True)
+
+    amount = forms.DecimalField(label='Amount', decimal_places=2, required=True)
+
+    description = forms.CharField(label='Description', max_length=200, required=True)
+
+    participant1 = forms.ChoiceField(label='Paid for expense',
+                                     choices=[(None, '')],
+                                     required=True)
+
+    participant2 = forms.ChoiceField(label='N/A',
+                                     choices=[(None, '')],
+                                     required=False)
+
+    allocations_toggle = forms.ChoiceField(label='Covers',
+                                           choices=[(0, 'Entire group'), (1, 'Select participants')],
+                                           initial=0,
+                                           required=True)
+
+    allocations = forms.MultipleChoiceField(label='Covers',
+                                            choices=[],
+                                            required=False,
+                                            widget=forms.CheckboxSelectMultiple())
 
     def __init__(self, event, *args, **kwargs):
         super(MoneyRecordForm, self).__init__(*args, **kwargs)
@@ -25,11 +47,16 @@ class MoneyRecordForm(forms.Form):
         assert type(event) is Event
         self.event = event
 
-        participant_choices = [(None, '')]
+        participant_choices = []
+        self._event_participant_ids = []
         for participant in event.participants():
             participant_choices.append((participant.id, participant.get_name()))
-        self.fields['participant1'].choices = participant_choices
-        self.fields['participant2'].choices = participant_choices
+            self._event_participant_ids.append(participant.id)
+
+        self.fields['participant1'].choices += participant_choices
+        self.fields['participant2'].choices += participant_choices
+        self.fields['allocations'].choices += participant_choices
+        self.fields['allocations'].initial = self._event_participant_ids
 
         # record type: expense (default) or transfer
         self.record_type = 'expense'
@@ -66,34 +93,51 @@ class MoneyRecordForm(forms.Form):
             self.fields['participant2'].required = True
             self.fields['participant2'].label = 'Transfer to'
 
+            self.fields['allocations_toggle'].required = False
+            self.fields['allocations_toggle'].label = 'N/A'
+
+            self.fields['allocations'].required = False
+            self.fields['allocations'].label = 'N/A'
+
         else:
             raise Exception('Invalid: ' + self.record_type)
 
-    def populate_from_record(self, record):
-        assert type(record) is MoneyRecord
-        assert record.event_id is self.event.id
+    def populate_from_record(self, money_record):
+        assert type(money_record) is MoneyRecord
+        assert money_record.event_id is self.event.id
 
-        assert record.pub_date
-        assert record.amount
-        assert record.description
-        assert record.participant1
-        if record.participant2:
+        assert money_record.pub_date
+        assert money_record.amount
+        assert money_record.description
+        assert money_record.participant1
+        if money_record.participant2:
             self.set_record_type('transfer')
         else:
             self.set_record_type('expense')
 
-        self.fields['date'].initial = record.pub_date
-        self.fields['amount'].initial = record.amount
+        self.fields['date'].initial = money_record.pub_date
+        self.fields['amount'].initial = money_record.amount
 
         if self.record_type == 'expense':
-            self.fields['description'].initial = record.description
-            self.fields['participant1'].initial = record.participant1.id
+            self.fields['description'].initial = money_record.description
+            self.fields['participant1'].initial = money_record.participant1.id
             self.fields['participant2'].initial = None
+
+            allocations = money_record.allocations()
+            if len(self._event_participant_ids) == len(allocations):
+                self.fields['allocations_toggle'].initial = 0
+                self.fields['allocations'].initial = self._event_participant_ids
+            else:
+                self.fields['allocations_toggle'].initial = 1
+                allocation_participant_ids = []
+                for allocation in allocations:
+                    allocation_participant_ids.append(allocation.participant_id)
+                self.fields['allocations'].initial = allocation_participant_ids
 
         elif self.record_type == 'transfer':
             self.fields['description'].initial = None
-            self.fields['participant1'].initial = record.participant1.id
-            self.fields['participant2'].initial = record.participant2.id
+            self.fields['participant1'].initial = money_record.participant1.id
+            self.fields['participant2'].initial = money_record.participant2.id
 
         else:
             raise Exception('Invalid: ' + self.record_type)
@@ -103,8 +147,9 @@ class MoneyRecordForm(forms.Form):
         cleaned_data = super(MoneyRecordForm, self).clean()
 
         if self.record_type == 'expense':
-            # no custom validation for expenses
-            pass
+            allocations = cleaned_data.get('allocations')
+            if len(allocations) < 1:
+                self.add_error('allocations', "Must select at least one")
 
         elif self.record_type == 'transfer':
             participant1_id = cleaned_data.get('participant1')
@@ -123,8 +168,22 @@ class MoneyRecordForm(forms.Form):
         else:
             raise Exception('Invalid: ' + self.record_type)
 
+    def _get_allocation_participant_ids(self):
+        assert self.record_type == 'expense', 'Unsupported operation'
+
+        allocations_toggle = self.cleaned_data['allocations_toggle']
+        assert allocations_toggle
+
+        if allocations_toggle == '0':
+            return self._event_participant_ids
+        elif allocations_toggle == '1':
+            return self.cleaned_data['allocations']
+        else:
+            raise Exception('Unhandled value: ' + str(allocations_toggle))
+
     def save_as_new_record(self):
         assert self.is_valid(), 'Form validation is a pre-requisite'
+
         with transaction.atomic():
             new_record = MoneyRecord.objects.create(
                 event=self.event,
@@ -138,22 +197,41 @@ class MoneyRecordForm(forms.Form):
 
             if self.record_type == 'expense':
                 # Create allocations for expense record
-                participants = self.event.participants()
-                assert len(participants) > 0
-                for participant in participants:
-                    new_allocation = Allocation.objects.create(money_record=new_record, participant=participant)
+                allocation_participant_ids = self._get_allocation_participant_ids()
+                assert len(allocation_participant_ids) > 0
+
+                for participant_id in allocation_participant_ids:
+                    new_allocation = Allocation.objects.create(money_record=new_record, participant_id=participant_id)
                     new_allocation.save()
 
-
-    def update_existing(self, record):
+    def update_existing(self, money_record):
         assert self.is_valid(), 'Form validation is a pre-requisite to this function'
-        assert type(record) is MoneyRecord
+        assert type(money_record) is MoneyRecord
 
         with transaction.atomic():
-            assert record.event_id is self.event.id
-            record.pub_date = self.cleaned_data['date']
-            record.description = self.cleaned_data['description']
-            record.amount = self.cleaned_data['amount']
-            record.participant1_id = self.cleaned_data['participant1']
-            record.participant2_id = self.cleaned_data['participant2']
-            record.save()
+            assert money_record.event_id is self.event.id
+            money_record.pub_date = self.cleaned_data['date']
+            money_record.description = self.cleaned_data['description']
+            money_record.amount = self.cleaned_data['amount']
+            money_record.participant1_id = self.cleaned_data['participant1']
+            money_record.participant2_id = self.cleaned_data['participant2']
+            money_record.save()
+
+            allocation_participant_ids = self._get_allocation_participant_ids()
+            assert len(allocation_participant_ids) > 0
+
+            # delete existing allocations that no longer hold
+            existing_allocations = money_record.allocations()
+            for existing_allocation in existing_allocations:
+                if existing_allocation.participant_id in allocation_participant_ids:
+                    allocation_participant_ids.remove(existing_allocation.participant_id)
+                else:
+                    existing_allocation.delete()
+
+            # add new allocations that do not exist yet
+            for participant_id in allocation_participant_ids:
+                new_allocation = Allocation.objects.create(money_record=money_record, participant_id=participant_id)
+                new_allocation.save()
+
+
+
